d92bbe517e1aa8d73b98a589a72a7adf315b498d
Aday Talavera <aday.talavera@gmail.com>
2013-06-14 12:01:53 +0100

    Added galician translation contribution and updated minor version number

---
+++ lang/gl/block_dedication.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['blockname'] = 'Dedicación ao curso';
+$string['blocktitle'] = 'Dedicación ao curso';
+$string['calculate'] = 'Calcular...';
+$string['calculatededication'] = 'Continuar';
+$string['dedication:use'] = 'Consultar dedicación ao curso';
+$string['dedication'] = 'Dedicación ao curso';
+$string['dedicationall'] = 'Membros do curso. Seleccione un para ver o detalle da súa dedicación ao curso <br> desde: {$a->strmintime} <br> ata:  {$a->strmaxtime}.';
+$string['downloaddedication'] = 'Descargar en formato Excel';
+$string['duration'] = 'Duración';
+$string['end'] = 'Fin do período';
+$string['hoursandminutes'] = '{$a->hours} horas e {$a->minutes} minutos';
+$string['limit'] = 'Límite entre clics (en minutos)';
+$string['minutes'] = '{$a} minutos';
+$string['nomembers'] = 'Non hai participantes no curso.';
+$string['select'] = 'Seleccione o rango de datas e o tempo máximo entre clics dunha mesma sesión';
+$string['sessionstart'] = 'Inicio da sesión';
+$string['showdedication'] = 'Amosar dedicación total dos membros';
+$string['start'] = 'Inicio do período';
+$string['totaldedication'] = '<b>Dedicación total: </b> {$a}';
+$string['userdedication'] = 'Dedicación ao curso de {$a->firstname} {$a->lastname} <br> desde: {$a->strmintime} <br> ata:  {$a->strmaxtime}.';
+
+// Moodle 2.0
+$string['pluginname'] = $string['blockname'];
+?>
