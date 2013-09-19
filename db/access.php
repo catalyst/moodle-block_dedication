a3e7e4424382a9e83de861d1f9d13846079b8b7b
Aday Talavera <aday.talavera@gmail.com>
2013-09-19 10:18:52 +0100

    Added default capability for block in Moodle 2.5. Bumped minor version number.

--- db/access.php
+++ db/access.php
@@ -19,10 +19,20 @@ $capabilities = array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
         )
     ),
-
+    'block/dedication:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
 );
 
 ?>
