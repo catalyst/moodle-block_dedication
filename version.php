6cb8a81463908f18a5e9e7f633d4fab1211fb8d4
Mahesh Murali P <maheshmuralip@gmail.com>
2025-07-29 10:59:57 +0100

    Updated plugin version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_dedication';
 $plugin->release = 2024072200;
-$plugin->version = 2024072200;
+$plugin->version = 2025072900;
 $plugin->requires = 2024042200; // Requires 4.4.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->supported = [404, 405]; // Updated: Add support for 4.5.
+$plugin->supported = [404, 405];
